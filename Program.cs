a4e20c0dd7252e054cee56cdc04bac5bf320282e
none_None1 <mxm20110612@163.com>
2023-09-07 20:37:25 +0800

    Upload repo

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using System.IO;
+namespace BrainFexec
+{
+    internal class Program
+    {
+        private static string code = "", fn = "";
+        static void Main(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 1:
+                    {
+                        code = File.ReadAllText(args[0]);
+                        string[] s = args[0].Split('\\');
+                        string [] S = s[s.Length - 1].Split('.');
+                        fn = S[0] + ".exe";
+                        break;
+                    }
+                case 2:
+                    {
+                        code = File.ReadAllText(args[0]);
+                        fn = args[1];
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Usage:\nBrainFExec <input> [output]\nBrainFuck to EXE compiler, compiles BrainFuck into a Windows executable.\ninput: Input file name\nOutput: Output file name, if this argument is not passed and the input file name is 'a.bf', output file name will be 'a.exe'.");
+                        return;
+                    }
+            }
+            CodeDomProvider compiler = CodeDomProvider.CreateProvider("CSharp");
+            CompilerParameters param = new CompilerParameters();
+            param.ReferencedAssemblies.Add("System.dll");
+            param.GenerateExecutable = true;
+            param.OutputAssembly = fn;
+            param.MainClass = "BF";
+            param.GenerateInMemory = true;
+            string t = code, brainfuck = "using System;\r\npublic class BF{\r\nprivate static int[] tape=new int[1000000];\r\nprivate static int p=0;\r\nstatic void Main(string[] args){\r\n";
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == '+')
+                {
+                    brainfuck += "tape[p]=(tape[p]==255?0:tape[p]+1);";
+                }
+                if (t[i] == '-')
+                {
+                    brainfuck += "tape[p]=(tape[p]==0?255:tape[p]-1);";
+                }
+                if (t[i] == ',')
+                {
+                    brainfuck += "tape[p]=Console.Read();";
+                }
+                if (t[i] == '.')
+                {
+                    brainfuck += "Console.Write(Convert.ToChar(tape[p]));";
+                }
+                if (t[i] == '>')
+                {
+                    brainfuck += "p++;";
+                }
+                if (t[i] == '<')
+                {
+                    brainfuck += "p--;";
+                }
+                if (t[i] == '[')
+                {
+                    brainfuck += "while(tape[p]!=0){";
+                }
+                if (t[i] == ']')
+                {
+                    brainfuck += "}";
+                }
+                brainfuck += "\r\n";
+            }
+            brainfuck += "}\r\n}";
+            CompilerResults result = compiler.CompileAssemblyFromSource(param, brainfuck);
+            if (result.Errors.HasErrors)
+            {
+                Console.WriteLine("Unmatched bracket");
+            }
+            else
+            {
+                Console.WriteLine("Done!");
+                Console.WriteLine("Successfully compiled to " + fn);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BrainFExec
- BrainFuck compiler to Windows executable in C#
+ BrainFExec is a [BrainFuck](https://esolangs.org/wiki/BrainFuck) compiler to Windows executable in C#.
+## Usage
+```commandline
+BrainFExec <input> [output]
+```
+Compiles BrainFuck code into a Windows executable:
+* input: Input BrainFuck file name
+* output: Output BrainFuck file name
+  
+If `output` is not passed, BrainFExec automatically decides the file name: For example, if the input BrainFuck filename is `a.bf`, it sets the output file to `a.exe`.
+## How it works
+It first converts BrainFuck to C#, then uses dynamic compiling to compile the resulting C# program to an executable.
